bef48fcff8d662f6b0241815abd5c1a256a7e11f
Tom <8162045+tbjgolden@users.noreply.github.com>
2022-04-18 14:45:40 +0100

    Initial commit from just-build

---
+++ cli/index.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import { hello } from "term-kitty-img";
+
+const [_cmd, _fileName, ...args] = process.argv;
+const arg = args.join(" ");
+
+if (arg !== "") {
+  console.log(hello(arg));
+}
---
+++ web/App.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { hello } from "term-kitty-img";
+
+export const App = () => <h1>{hello("Tom")}</h1>;
